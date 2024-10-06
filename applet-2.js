fe99aa59834462f7982fb80e71e0b5c969b9f728
aivandave123504 <20232309@nbsc.edu.ph>
2024-10-06 23:20:03 +0800

    Added loadmarker leaflet map

--- applet-2.js
+++ applet-2.js
@@ -103,3 +103,13 @@ class LeafletMap {
 
 }
 
+const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
+
+
+Mymap.loadMarkersFromJson('applet-2.json');
+
+document.addEventListener('DOMContentLoaded', () => {
+    Mymap.displayLogCount();
+    Mymap.loadMarkersFromJson('applet-2.json');
+});
+
