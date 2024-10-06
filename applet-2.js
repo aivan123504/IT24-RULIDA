88c8b98bffbe01dfa82508ea0a88601ac7ed6a0e
aivandave123504 <20232309@nbsc.edu.ph>
2024-10-06 23:03:50 +0800

    Added markercounts

--- applet-2.js
+++ applet-2.js
@@ -3,3 +3,8 @@ class LeafletMap {
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
+
+        this.markerCounts = {};
+        this.markers = [];
+
+        this.loggedData = []; 
