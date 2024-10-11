02a0feafe4e543547069f99d582d3c9f491823a7
aivandave123504 <20232309@nbsc.edu.ph>
2024-10-11 20:41:28 +0800

    Added class cardsearch

---
+++ index.js
@@ -0,0 +1,8 @@
+class CardSearch {
+    constructor(searchInputId, cardContainerClass) {
+        this.searchInput = document.getElementById(searchInputId);
+        this.cardContainer = document.querySelector(`.${cardContainerClass}`);
+        this.cards = this.cardContainer.querySelectorAll('.card');
+
+        this.init();
+    }
